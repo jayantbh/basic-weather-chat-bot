3457fbb9b7552f42a50780351862396f1b465457
Jayant Bhawal <bhawal.jayant@gmail.com>
2016-05-08 04:29:59 +0530

    Working on "message us" button.

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
+var yql = require('yql');
+var Wit = require('node-wit').Wit;
 
 var token = process.env.FB_TOKEN;
+var wit_token = process.env.WIT_TOKEN;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
@@ -32,6 +35,21 @@ function sendTextMessage(sender, text) {
     });
 }
 
+const actions = {
+    say(sessionId, context, message, cb) {
+        console.log(message);
+        cb();
+    },
+    merge(sessionId, context, entities, message, cb) {
+        cb(context);
+    },
+    error(sessionId, context, err) {
+        console.log(err.message);
+    }
+};
+const client = new Wit(wit_token, actions);
+var context = {};
+
 //WebHook validator
 app.get("/webhook", function (req, res) {
     if (req.query['hub.verify_token'] === process.env.HUB_TOKEN) {
@@ -49,10 +67,20 @@ app.post('/webhook', function (req, res) {
         event = req.body.entry[0].messaging[i];
         sender = event.sender.id;
         console.log(event);
-        if (event.message && event.message.text) {
-            text = event.message.text;
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
-            console.log(text);
+
+        context[sender] = context[sender] || {};
+        if(event.optin){
+            sendTextMessage(sender, "Hey there! Call me Jay. To see a list of things you can do on Jayant's Experiments, type 'help'.");
+        }
+        // else if (event.message && event.message.text) {
+        //     text = event.message.text;
+        //     sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
+        //     console.log(text);
+        // }
+        else {
+            sendTextMessage(sender, "Damn. This action seems to be unhandled for now. Message m.me/jayantbhawal about this.");
+            sendTextMessage(sender, "Event body:");
+            sendTextMessage(sender, req.body);
         }
     }
     res.sendStatus(200);
